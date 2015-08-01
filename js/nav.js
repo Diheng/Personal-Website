06042f40b08c6b46fdbb3bfd4437b70a5e2f22e0
Diheng Zhang <dihengz@gmail.com>
2015-08-01 18:10:48 -0400

    basic pages setup

---
+++ js/nav.js
@@ -0,0 +1,42 @@
+document.write('  <div class="col-md-3">
+  <div class="sidebar-nav">
+  <nav class="navbar navbar-default" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-navbar-collapse">	  
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <span class="navbar-brand" href="#">Brand</span>
+  </div>
+
+  <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="navbar-collapse collapse sidebar-navbar-collapse">  
+    <ul class="nav nav-pills">
+	  <li class="active"><a href="index.html">About</a></li>
+      <li><a href="project.html">Project</a></li>
+	  <li><a href="publication.html">Publication</a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">CV/Resume<b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">CV</a></li>
+          <li><a href="#">Resume</a></li>
+        </ul>
+      </li>
+	  <li><a href="findme.html">FindMe@</a></li>
+	  <li><a href-"/cn/index.html">中文</a></li>
+    </ul>
+	
+    <form class="navbar-form navbar-left" role="search">
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="Search">
+      </div>
+      <button type="submit" class="btn btn-default">Submit</button>
+    </form>
+  </div><!-- /.navbar-collapse -->
+</nav>
+</div>
+</div>
+');
